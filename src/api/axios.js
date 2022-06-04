d33f69d67fb84ed08443b8aa327c3bcfb42781b6
SanyInc <sany.inspections@gmail.com>
2022-06-04 18:21:33 +0300

    url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://sany-inspections.herokuapp.com/api/",
   timeout: 10000,
   headers: {
     "Content-type": "application/json",
